49f6b1fcc7f6418d4a006d016f84df16f924b98b
raghavptdr18 <88372112+raghavptdr18@users.noreply.github.com>
2022-11-11 16:52:37 +0530

    Update algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -1,17 +1,73 @@
-// CS-302 Analysis and design of algorithm
+// CS-302 Analysis and design of algorithm project
 
-#include <bits/stdc++.h>
+// A new Karzanov-type algorith O(n^3) max-flow algorithm
+// Submitted By -
+/*
+    Raghav Patidar - 2020CSB1115
+    Raj            - 2020CSB1116
+    Rohan          - 2020CSB1117
+    Janmeet Singh  - 2020CSB1175
+    Jyoti Dhiman   - 2020CSB1092
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-#define mn 1000
+#define mn 1000 // Maximum nodes
 
-int n;
-int cap[mn][mn];
-int flow[mn][mn];
-int level[mn];
-pair<int, int> pr[mn];
+int n;                  // Total Number of Nodes in Graph
+int cap[mn][mn];        // Capacity of each edge
+int flow[mn][mn];       // Flow through particular edge
+int level[mn];          // Level of nodes after making of layered network Acyclic Structure
 
-void initialize()
+struct node {          // Node description
+    int data;
+    node* next;
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+struct Queue {         // Queue data Structure from Scratch
+    node *front, *rear;
+    Queue() { front = rear = NULL; }
+
+    void enQueue(int x)      // Enqueue Function
+    {
+        node* temp = new node(x);
+        if (rear == NULL) {
+            front = rear = temp;
+            return;
+        }
+        rear->next = temp;
+        rear = temp;
+    }
+    void deQueue()        // Dequeue Function
+    {
+        if (front == NULL)
+            return;
+        node* temp = front;
+        front = front->next;
+        if (front == NULL)
+            rear = NULL;
+        delete (temp);
+    }
+};
+
+struct pairs{     // Pair data structure From scratch
+    int lvl;
+    int node;
+    pairs(int l, int n){
+        lvl = l;
+        node = n;
+    }
+};
+
+void initialize()   // Initializing capacity matrix (cap) and flow matrix (flow) and level
 {
     for (int i = 0; i < mn; i++)
     {
@@ -21,46 +77,41 @@ void initialize()
             cap[i][j] = 0;
             flow[i][j] = 0;
         }
-    }
 }
 
-void assign_level(int src)
-{
-    queue<int> q;
-    level[src] = 0;
-    q.push(src);
-    while (!q.empty())
-    {
-        int x = q.front();
-        q.pop();
+    }
 
+void assign_level(int src)       // Creating layered acyclic network and assigning levels to each node
+{
+    Queue q ;
+    level[src] = 0;
+    q.enQueue(src);
+    while (q.front != NULL)
+    {
+        int x = (q.front)->data;
+        q.deQueue();
         for (int j = 1; j <= n; j++)
         {
             if (cap[x][j]>0)
             {
                 if (level[j] < level[x] + 1)
                     level[j] = level[x] + 1;
-                q.push(j);
+                q.enQueue(j);
             }
         }
     }
-
-    for (int i = 0; i <= n; i++)
-    {
-        cout << level[i] << " ";
-    }
-    cout << endl;
 }
 
 int main()
 {
-    initialize();
+    initialize();            // calling initializing function
     cout << "Enter number of vertices: ";
     cin >> n;
-    int incoming_cap[n + 1];
-    int outgoing_cap[n + 1];
 
-    for (int i = 0; i <= n ; i++)
+    int incoming_cap[n + 1];          // sum of total incoming flow of a particular node
+    int outgoing_cap[n + 1];          // sum of total outgoing flow of a particular node
+
+    for (int i = 0; i <= n ; i++)  // initializing to zero
     {
         incoming_cap[i] = 0;
         outgoing_cap[i] = 0;
@@ -83,47 +134,55 @@ int main()
         }
         cin >> right >> w;
         cap[left][right] = w;
-        cap[right][left] = -w; // for par identification
+        cap[right][left] = -w; // for parent identification
 
         cout << "Inserted Edge !!\n";
     }
+    struct pairs *pr[n]; // array of pairs for storing {level,node}
+
     int source, sink;
     cout << "Enter source vertex : ";
     cin >> source;
     cout << "Enter sink vertex : ";
     cin >> sink;
-
     assign_level(source);
+    struct pairs *tt = new pairs(0, 0);
+    pr[0] = tt ;
 
-    for (int i = 1; i <= n; i++)
-    {
-        pr[i] = {level[i], i};
+    int k = 1 ;
+    for(int i = -1; i <= n ; i++){
+        for(int j = 1 ; j <= n; j++){
+            if(level[j]==i){
+                struct pairs *temp = new pairs(i, j);
+                pr[k] = temp;
+                ++k;
+            }
+        }
     }
 
-    int balanced[n + 1];
-    for (int i = 0; i <= n; i++)
+    int balanced[n + 1];       // check for balanced node
+    int blocked[n + 1];        // check for blocked node
+
+    for (int i = 0; i <= n; i++) // initializing to zero
+    {
         balanced[i] = 0;
+        blocked[i]=0;
+    }
 
-    int blocked[n + 1];
-    for (int i = 0; i <= n; i++)
-        blocked[i] = 0;
+    int is_balanced = 0; // check for balanced condition of every node
+    int f = 1;     // f==1 means algorithm is in forward step else it is in backward step
 
-    int is_balanced = 0;
-    int f = 1;
-    sort(pr, pr + n + 1);
-
-    balanced[source]=1;
-    balanced[sink]=1;
-    blocked[source]=0;
-    blocked[sink]=0;
+    balanced[source]=1; // source is always balanced
+    balanced[sink]=1;    // sink is always balanced
+    blocked[source]=0;   // source is always unblocked
+    blocked[sink]=0;     // sink is always unblocked
 
     while (!is_balanced)
     {
-        // Forward
-
+        // Forward step
         if (f == 1)
         {
-            // step 1 :
+            // step 1 : Maximizing flow to its capacity of an edge except edge between source node and blocked node
 
             for (int i = 1; i <= n; i++)
             {
@@ -142,11 +201,16 @@ int main()
 
             for (int i = 1; i <= n; i++)
             {
-                int lv = pr[i].first;
-                int nd = pr[i].second;
-
+                int lv = pr[i]->lvl;
+                int nd = pr[i]->node;
                 if (nd==source || nd==sink)
                     continue;
+                if(lv==-1)
+                {
+                    balanced[nd]=1;
+                    blocked[nd]=0;
+                    continue;
+                }
 
                 if (incoming_cap[nd] > outgoing_cap[nd])
                 {
@@ -246,13 +310,13 @@ int main()
                 }
             }
 
-            f=0;
+            f=0;  // making forward to zero it means now our algorithm goes in backward direction
         }
 
-        ///// Backward
+        ///// Backward Direction
         else
         {
-             // step 1 :
+            // step 1 : Maximizing flow to its capacity of an edge except edge between blocked node and sink node
             for (int i = 1; i <= n; i++)
             {
                 if (cap[sink][i] < 0 && blocked[i]==0)
@@ -266,16 +330,21 @@ int main()
                 }
             }
 
-            // step 2 : from n-1 level
+            // step 2 : from second last level to level 1
 
             for (int i = n; i >= 1; i--)
             {
-                int nd = pr[i].second;
-                int lv = pr[i].first;
-
+                int nd = pr[i]->node;
+                int lv = pr[i]->lvl;
                 if(nd==source || nd==sink) continue;
+                if(lv==-1)
+                {
+                    balanced[nd]=1;
+                    blocked[nd]=0;
+                    continue;
+                }
 
-                if (incoming_cap[nd] < outgoing_cap[nd])  ///////////////////////////////////////////
+                if (incoming_cap[nd] < outgoing_cap[nd])
                 {
                     int to_increase = outgoing_cap[nd] - incoming_cap[nd];
 
@@ -314,7 +383,7 @@ int main()
                         }
 
                     }
-                    if (incoming_cap[nd] != outgoing_cap[nd])
+                    if (incoming_cap[nd] != outgoing_cap[nd]) // even now if node is not balanced we marked it blocked and unbalanced
                     {
                         blocked[nd] = 1;
                         balanced[nd]=0;
@@ -359,7 +428,7 @@ int main()
                         }
                     }
 
-                    if(incoming_cap[nd]!=outgoing_cap[nd])
+                    if(incoming_cap[nd]!=outgoing_cap[nd])   // even now if node is not balanced we marked it blocked and unbalanced
                     {
                         blocked[nd]=1;
                         balanced[nd]=0;
@@ -375,7 +444,7 @@ int main()
 
         }
 
-        // Check for all balance
+        // Check for all balance nodes
 
         int c = 0;
         for (int i = 1; i <= n; i++)
@@ -385,10 +454,15 @@ int main()
                 c = 1;
                 break;
             }
-
         }
-        if (c == 0) is_balanced = 1;
+        if (c == 0) is_balanced = 1;  // if all nodes are balanced we come out of loop and our job is done
     }
-    cout<<"Complete....huehue"<<endl;
+
+    int max_flow = 0 ;
+    for(int i = 1 ; i <= n ; i++){
+        max_flow += flow[i][sink];
+    }
+    cout<<endl;
+    cout<<" MAXIMUM FLOW IS : " << max_flow <<endl;
 
 }
